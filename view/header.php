5b1e399d5c07ef05be019008825a24702ac72c75
Nam704 <daovannam2k4@gmail.com>
2024-03-10 13:27:05 +0700

    chuan bị tai nguyen

---
+++ view/header.php
@@ -0,0 +1,259 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<!-- Mirrored from www.jthemes.net/themes/html/neoncart/home_supermarket.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 09 Mar 2024 16:54:30 GMT -->
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+    <title>Supermarket - Neoncart HTML5 Template</title>
+    <link rel="shortcut icon" href="./assets/images/logo/favourite_icon_01.png">
+
+    <!-- fraimwork - css include -->
+    <link rel="stylesheet" type="text/css" href="./assets/css/bootstrap.min.css">
+
+    <!-- icon - css include -->
+    <link rel="stylesheet" type="text/css" href="./assets/css/fontawesome.css">
+
+    <!-- animation - css include -->
+    <link rel="stylesheet" type="text/css" href="./assets/css/animate.css">
+
+    <!-- nice select - css include -->
+    <link rel="stylesheet" type="text/css" href="./assets/css/nice-select.css">
+
+    <!-- carousel - css include -->
+    <link rel="stylesheet" type="text/css" href="./assets/css/slick.css">
+    <link rel="stylesheet" type="text/css" href="./assets/css/slick-theme.css">
+
+    <!-- popup images & videos - css include -->
+    <link rel="stylesheet" type="text/css" href="./assets/css/magnific-popup.css">
+
+    <!-- jquery ui - css include -->
+    <link rel="stylesheet" type="text/css" href="./assets/css/jquery-ui.css">
+
+    <!-- custom - css include -->
+    <link rel="stylesheet" type="text/css" href="./assets/css/style.css">
+
+</head>
+
+
+<body class="home_supermarket">
+
+
+    <!-- backtotop - start -->
+    <div id="thetop"></div>
+    <div class="backtotop bg_supermarket_red">
+        <a href="#" class="scroll">
+            <i class="far fa-arrow-up"></i>
+        </a>
+    </div>
+    <!-- backtotop - end -->
+
+    <!-- preloader - start -->
+    <!-- <div id="preloader"></div> -->
+    <!-- preloader - end -->
+
+
+    <!-- header_section - start
+		================================================== -->
+    <header class="header_section supermarket_header bg-white clearfix">
+        <div class="header_top text-white clearfix">
+            <div class="container maxw_1460">
+                <div class="row align-items-center justify-content-lg-between">
+                    <div class="col-lg-5">
+                        <p class="welcome_text mb-0">Welcome to Worldwide Online marketplace Store</p>
+                    </div>
+
+                    <div class="col-lg-7">
+                        <ul class="info_list ul_li_right clearfix">
+                            <li><a href="#!"><i class="fal fa-map-marker-alt"></i> Store Locator</a></li>
+                            <li><a href="#!"><i class="fal fa-truck"></i> Track Your Order</a></li>
+                            <li>
+                                <form action="#">
+                                    <div class="currency_select option_select mb-0">
+                                        <select>
+                                            <option value="USD" selected>US Dollars</option>
+                                            <option value="EUR">Euro</option>
+                                            <option value="GBP">UK Pounds</option>
+                                        </select>
+                                    </div>
+                                </form>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="header_middle clearfix">
+            <div class="container maxw_1460">
+                <div class="row align-items-center justify-content-lg-between">
+                    <div class="col-lg-3">
+                        <div class="brand_logo">
+                            <a class="brand_link" href="index.html">
+                                <img src="./assets/images/logo/logo_18_1x.png" srcset="./assets/images/logo/logo_18_2x.png 2x" alt="logo_not_found">
+                            </a>
+
+                            <ul class="mh_action_btns ul_li clearfix">
+                                <li>
+                                    <button type="button" class="search_btn" data-toggle="collapse" data-target="#search_body_collapse" aria-expanded="false" aria-controls="search_body_collapse">
+                                        <i class="fal fa-search"></i>
+                                    </button>
+                                </li>
+                                <li>
+                                    <button type="button" class="cart_btn">
+                                        <i class="fal fa-shopping-cart"></i>
+                                        <span class="btn_badge">2</span>
+                                    </button>
+                                </li>
+                                <li><button type="button" class="mobile_menu_btn"><i class="far fa-bars"></i></button>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-6">
+                        <form action="#">
+                            <div class="medical_search_bar">
+                                <div class="form_item">
+                                    <input type="search" name="search" placeholder="search here...">
+                                </div>
+                                <div class="option_select mb-0">
+                                    <select>
+                                        <option data-display="All Category">Select A Option</option>
+                                        <option value="1">Some option</option>
+                                        <option value="2">Another option</option>
+                                        <option value="3" disabled>A disabled option</option>
+                                        <option value="4">Potato</option>
+                                    </select>
+                                </div>
+                                <button type="submit" class="submit_btn"><i class="fal fa-search"></i></button>
+                            </div>
+                        </form>
+                    </div>
+
+                    <div class="col-lg-3">
+                        <div class="supermarket_header_btns clearfix">
+                            <ul class="action_btns_group ul_li_right clearfix">
+                                <li>
+                                    <button type="button">
+                                        <span>Need</span>
+                                        <strong>Help?</strong>
+                                    </button>
+                                </li>
+                                <li>
+                                    <button type="button">
+                                        <span>Your</span>
+                                        <strong>Account</strong>
+                                    </button>
+                                </li>
+                                <li>
+                                    <button type="button" class="cart_btn">
+                                        <i class="fal fa-shopping-bag"></i>
+                                        <span class="btn_badge">2</span>
+                                    </button>
+                                </li>
+                            </ul>
+                            <span class="alart_text float-right">
+                                <small>£</small>
+                                New here? Get your Coupons!
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="header_bottom clearfix">
+            <div class="container maxw_1460">
+                <nav class="main_menu clearfix">
+                    <ul class="ul_li clearfix">
+                        <li>
+                            <button class="alldepartments_btn bg_supermarket_red text-uppercase" type="button" data-toggle="collapse" data-target="#alldepartments_dropdown" aria-expanded="false" aria-controls="alldepartments_dropdown">
+                                <i class="far fa-bars"></i> All Departments
+                            </button>
+                        </li>
+                        <li class="menu_item_has_child">
+                            <a href="#!">Home</a>
+                            <div class="mega_menu text-center">
+                                <div class="background" data-bg-color="#ffffff">
+                                    <div class="container">
+                                        <ul class="home_pages ul_li clearfix">
+
+                                            <li>
+                                                <a href="home_supermarket.html">
+                                                    <span class="item_image">
+                                                        <img src="./assets/images/home_pages/supermarket.png" alt="image_not_found">
+                                                    </span>
+                                                    <span class="item_title">Supermarket</span>
+                                                </a>
+                                            </li>
+
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+
+                        <li class="menu_item_has_child">
+                            <a href="#!">Pages</a>
+                            <ul class="submenu">
+                                <li class="menu_item_has_child">
+                                    <a href="#!">Shop Inner Pages</a>
+                                    <ul class="submenu">
+                                        <li><a href="shop_cart.html">Shopping Cart</a></li>
+                                        <li><a href="shop_checkout.html">Checkout Step 1</a></li>
+                                        <li><a href="shop_checkout_step2.html">Checkout Step 2</a></li>
+                                        <li><a href="shop_checkout_step3.html">Checkout Step 3</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="404.html">404 Page</a></li>
+                                <li class="menu_item_has_child">
+                                    <a href="#!">Blogs</a>
+                                    <ul class="submenu">
+                                        <li><a href="blog.html">Blog Page</a></li>
+                                        <li><a href="blog_details.html">Blog Details</a></li>
+                                    </ul>
+                                </li>
+                                <li class="menu_item_has_child">
+                                    <a href="#!">Compare</a>
+                                    <ul class="submenu">
+                                        <li><a href="compare_1.html">Compare V.1</a></li>
+                                        <li><a href="compare_2.html">Compare V.2</a></li>
+                                    </ul>
+                                </li>
+                                <li class="menu_item_has_child">
+                                    <a href="#!">Register</a>
+                                    <ul class="submenu">
+                                        <li><a href="login.html">Login</a></li>
+                                        <li><a href="signup.html">Sign Up</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+                        <li><a href="#!">About us</a></li>
+                        <li><a href="contact.html">Contact us</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+
+        <div id="search_body_collapse" class="search_body_collapse collapse">
+            <div class="search_body">
+                <div class="container-fluid prl_90">
+                    <form action="#">
+                        <div class="form_item mb-0">
+                            <input type="search" name="search" placeholder="Type here...">
+                            <button type="submit"><i class="fal fa-search"></i></button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- header_section - end
+		================================================== -->
